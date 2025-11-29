73b7ef4108d9543b7663490c264ded0a0b7ab9fd
Arifur999 <arr96777@gmail.com>
2025-11-29 23:08:03 +0600

    connect pg

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,37 @@
-import express,{Request,Response} from 'express';
-const app = express()
-const port = 5000
+import express, { Request, Response } from "express";
+import { Pool} from "pg";
 
-app.get('/', (req :Request, res:Response) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 5000;
+
+
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!, // non-null assertion
+  // যদি certificate সমস্যা আসে, নিম্নলিখিত ssl object use করতে পারো:
+  // ssl: {
+  //   rejectUnauthorized: false
+  // }
+});
+
+
+
+//parser============> middle ware
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  res.status(201).json({
+    success: true,
+    message: "API Is Working",
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
