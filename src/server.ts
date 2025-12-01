b5113a8d35e893aa1a0630afcf1bef9b403b91f0
Arifur999 <arr96777@gmail.com>
2025-12-01 21:32:55 +0600

    db clean code

--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
-import dotenv from "dotenv"; 
-import { Pool} from "pg";
-import path from "path";
 
 
-dotenv.config({path:path.join(process.cwd(),".env")});
+import config from "./config";
+import initDB, { pool } from "./config/db";
+
+
 const app = express();
-const port = 5000;
+const port = config.port;
 
 
-
-const pool = new Pool({
-  connectionString: `${process.env.DATABASE_URL}`
-});
-
-
-const initDB=async()=>{
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL,
-        age INT,
-        phone VARCHAR (15), 
-        address TEXT,
-        create_at TIMESTAMP DEFAULT NOW (),
-        update_at TIMESTAMP DEFAULT NOW ()
-
-        
-        )
-        
-        
-        `);
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS todos(
-        id SERIAL PRIMARY KEY,
-        users_id INT REFERENCES users(id) ON DELETE CASCADE,
-        title VARCHAR(200) NOT NULL,
-        description TEXT,
-        completed BOOLEAN DEFAULT false,
-        due_date DATE,
-        create_at TIMESTAMP DEFAULT NOW (),
-        update_at TIMESTAMP DEFAULT NOW ()
-        )
-        
-        
-        `)
-}
 
 initDB()
 //parser============> middle ware
